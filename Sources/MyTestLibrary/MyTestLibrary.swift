558cf1f7eba1669f1e0b40dbb41204776819beba
Paul Stinchcombe <paul@dragonsoftcommunications.com>
2022-05-31 15:36:58 +0800

    First upload

--- Sources/MyTestLibrary/MyTestLibrary.swift
+++ Sources/MyTestLibrary/MyTestLibrary.swift
@@ -2,5 +2,10 @@ public struct MyTestLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        self.text = "Paul is cool!!!"
+    }
+    
+    public func getData() -> String {
+        return self.text
     }
 }
